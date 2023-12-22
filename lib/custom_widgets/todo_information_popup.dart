3a561fa23e436039cbd918a2dc5f2c3d1ae78c3b
Iza <qianying.cao@stud.fhgr.ch>
2023-12-22 14:19:30 +0100

    bug fixes2.0

--- lib/custom_widgets/todo_information_popup.dart
+++ lib/custom_widgets/todo_information_popup.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
+//import 'package:intl/intl.dart';
 
 class TodoInformationPopup extends StatefulWidget {
   final TextEditingController titleController;
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -75,7 +75,7 @@ class _HomePageState extends State<HomePage> {
         children: [
           const SizedBox(height: 15),
           HorizontalDayList(dayUpdateFunction: changeWeekday),
-          HorizontalDateList(dayUpdateFunction: (day) {}),
+          HorizontalDateList(dayUpdateFunction: changeWeekday),
           const SizedBox(height: 20),
           Expanded(
             child: Container(
