516d81ab6094ee67e2d118c92d55af3b63a905a5
Iza <qianying.cao@stud.fhgr.ch>
2024-01-11 00:11:16 +0100

    Weitere Kommentare bei home.dart ergänzt

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,17 +5,19 @@ import 'package:todo_try/custom_widgets/todo_information_popup.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert'; // Für die JSON-Kodierung
 
+// Definition der Klasse Lernziel mit ihren Eigenschaften und Methoden
 class Lernziel {
   String titel;
   String beschreibung;
   DateTime datum;
 
+// Konstruktor für die Klasse Lernziel
   Lernziel({
     required this.titel,
     required this.beschreibung,
     required this.datum,
   });
-
+// Factory-Methode zur Erstellung eines Lernziels aus einem JSON-Objekt
   factory Lernziel.fromJson(Map<String, dynamic> json) {
     return Lernziel(
       titel: json['titel'],
@@ -23,7 +25,7 @@ class Lernziel {
       datum: DateTime.parse(json['datum']),
     );
   }
-
+// Methode zum Konvertieren des Lernziels in ein JSON-Objekt
   Map<String, dynamic> toJson() => {
         'titel': titel,
         'beschreibung': beschreibung,
@@ -31,6 +33,7 @@ class Lernziel {
       };
 }
 
+// Definition der Klasse HomePage
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -38,12 +41,14 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
+// Der Zustand für HomePage
 class _HomePageState extends State<HomePage> {
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
   TextEditingController dateController =
       TextEditingController(); // Neuer Controller für das Datum
 
+// Listen zur Speicherung der Lernziele
   List<Lernziel> todoInformation = [];
   List<Lernziel> dayDependentTodos = [];
   DateTime currentdate = DateTime.now();
@@ -76,6 +81,7 @@ class _HomePageState extends State<HomePage> {
     await prefs.setString('lernziele', lernzieleString);
   }
 
+// Hilfsfunktion zur Anzeige einer Nachricht in einem SnackBar
   void showInSnackBar(String value) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
@@ -90,6 +96,7 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
+// Funktion zum Ändern des aktuellen Datums
   void changeWeekday(DateTime newdate) {
     setState(() {
       currentdate = newdate;
@@ -97,6 +104,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+// Funktion zum Hinzufügen eines neuen Lernziels
   void addLernziel(DateTime chosenDate, String title, String description) {
     setState(() {
       todoInformation.add(Lernziel(
@@ -108,6 +116,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+// Funktion zum Aktualisieren der Liste basierend auf dem aktuellen Datum
   void updateList() {
     dayDependentTodos.clear();
     for (Lernziel lernziel in todoInformation) {
@@ -117,6 +126,7 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  // Funktion zum Navigieren für die nächste Woche
   void nextweek() {
     setState(() {
       currentdate = currentdate.add(const Duration(days: 7));
@@ -124,6 +134,7 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+// Funktion zum Navigieren für die Woche davor
   void previousweek() {
     setState(() {
       currentdate = currentdate.subtract(const Duration(days: 7));
@@ -133,34 +144,41 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // Erstellt Haupt-UI mit Scaffold Widget
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 111, 112, 112),
         appBar: AppBar(
           centerTitle: true,
           backgroundColor: const Color.fromARGB(255, 111, 112, 112),
-          elevation: 0.0,
+          elevation: 0.0, //Schatten unter App Bar entfernt
           title: const Text("LERNAPP"),
         ),
         body: Column(
           children: [
             const SizedBox(height: 15),
+            //Widget zur Anzeige einer horizontalen Liste von Tagen
             HorizontalDayList(
-//                key: dayListKey,
-                dayUpdateFunction: changeWeekday,
+//    löschen?            key: dayListKey,
+                dayUpdateFunction:
+                    changeWeekday, // Funktion wird aufgerufen, wenn Tag ausgewählt wird
                 startDate: currentdate),
             const SizedBox(height: 20),
             Expanded(
               child: Container(
-                width: double.infinity,
+                // cntainer für Darstellung der Lernziele
+                width: double.infinity, //Container nimt volle Breite ein
                 decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(30),
                     topRight: Radius.circular(30),
+                    // macht Ecken oben abgerundet
                   ),
                   boxShadow: [BoxShadow(blurRadius: 10.0)],
                 ),
-                child: TodoGridView(todoList: dayDependentTodos),
+                child: TodoGridView(
+                    todoList:
+                        dayDependentTodos), // Übergibt die tagesabhängigen Lernziele
               ),
             ),
           ],
@@ -179,10 +197,13 @@ class _HomePageState extends State<HomePage> {
               onPressed: nextweek,
               elevation: 5.0,
               backgroundColor: const Color.fromARGB(255, 158, 158, 158),
-              child: const Icon(Icons.arrow_forward, size: 35),
+              child: const Icon(Icons.arrow_forward,
+                  size: 35), // Button für die Navigation zur nächsten Woche
             ),
+            // Button zum Hinzufügen eines neuen Lernziels
             FloatingActionButton(
               onPressed: () {
+                // Öffnet ein Popup-Fenster zur Eingabe von Lernzielinformationen
                 showDialog<Map<String, dynamic>>(
                   context: context,
                   builder: (context) {
@@ -194,15 +215,18 @@ class _HomePageState extends State<HomePage> {
                 ).then((result) {
                   // 'selectedDate' empfängt das Datum vom Popup
                   if (result != null) {
+                    // Wenn das Ergebnis nicht null ist, wird ein neues Lernziel hinzugefügt
                     DateTime selectedDate = result['selectedDate'];
                     String title = result['title'];
                     String description = result['description'];
 
                     addLernziel(selectedDate, title, description);
                     saveLernziele();
+                    // Leert die Textfeld-Controller
                     titleController.clear();
                     descriptionController.clear();
                   } else {
+                    // Zeigt eine Nachricht, wenn das Popup-Ergebnis null ist
                     showInSnackBar(
                         "Bitte füllen Sie alle Felder aus und wählen Sie ein Datum.");
                   }
